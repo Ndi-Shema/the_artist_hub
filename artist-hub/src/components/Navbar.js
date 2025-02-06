bf94c981b47b715501732fd21099178b109f5b93
Ndi-Shema <f.shema@alustudent.com>
2025-02-06 21:14:45 +0200

    navbar

---
+++ artist-hub/src/components/Navbar.js
@@ -0,0 +1,29 @@
+// src/components/Navbar.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Nav = styled.nav`
+  background-color: #333;
+  color: white;
+  padding: 10px;
+`;
+
+const NavLink = styled(Link)`
+  color: white;
+  text-decoration: none;
+  margin: 0 15px;
+`;
+
+const Navbar = () => {
+  return (
+    <Nav>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/art-pieces">Art Pieces</NavLink>
+      <NavLink to="/login">Login</NavLink>
+      <NavLink to="/register">Register</NavLink>
+    </Nav>
+  );
+};
+
+export default Navbar;
